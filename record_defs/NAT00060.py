b8f1b9482d52236de0860c0f399691e44a1bc175
felix <darkpaw@internode.on.net>
2016-08-27 05:53:07 +1000

    00060 file def

---
+++ record_defs/NAT00060.py
@@ -0,0 +1,14 @@
+
+NAT00060 = [
+    # FIELDS – SUBJECT (NAT00060) FILE POSITION LENGTH TYPE
+    ["Subject flag", 1, 1, 'A'],
+    ["Subject identifier", 2, 12, 'A'],
+    ["Subject name", 14, 100, 'A'],
+    ["Subject field of education identifier", 114, 6, 'A'],
+    ["VET flag", 120, 1, 'A'],
+    ["Nominal hours", 121, 4, 'N'],
+
+]
+
+# Record length for national data collection: 124
+# Carriage return/line feed (ASCII 13/10): 2
